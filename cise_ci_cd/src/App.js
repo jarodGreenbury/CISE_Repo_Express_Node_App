8098ea75c47a274deb8b63ef6a15d16aeb77c6ac
jarodGreenbury <jarodGreenbury@users.noreply.github.com>
2021-03-25 16:52:49 +1300

    first commit

---
+++ cise_ci_cd/src/App.js
@@ -0,0 +1,27 @@
+// src/App.js
+import React, { Component } from 'react';
+class App extends Component {
+constructor() {
+super();
+this.state = {
+count: 0,
+}
+}
+makeIncrementer = amount => () =>
+this.setState(prevState => ({
+count: prevState.count + amount,
+}));
+increment = this.makeIncrementer(1);
+render() {
+return (
+<div>
+ENSE701 Assessment 1A
+S1 2021 Pg 3 of 6 copyright Jim Buchan
+<p>Count: {this.state.count}</p>
+<button className="increment" onClick={this.increment}>Increment
+count</button>
+</div>
+)
+}
+}
+export default App;
